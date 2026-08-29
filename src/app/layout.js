88362f1a0c3f1b8c749fd582065321390756d68e
sakibulislam75 <sakibulislam1623@gmail.com>
2026-08-29 22:15:29 +0600

    feat: add google dns server

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,9 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { ToastContainer, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import dns from 'node:dns';
+
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const josefinSans = Josefin_Sans({
    subsets: ['latin'],
